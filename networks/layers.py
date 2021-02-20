Fixed equalized learning rate + added low memory covariance estimation

networks/layers.py
@@ -1,37 +1,89 @@
-import numpy as np
 import tensorflow as tf
 from tensorflow.keras import backend
 from tensorflow.keras.layers import Add
 from tensorflow.keras.layers import Layer
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.initializers import RandomNormal
 
 
-class DenseEQL(Dense):
+class DenseEQL(Layer):
 
-    def __init__(self, gain=np.sqrt(2), **kwargs):
-        super().__init__(kernel_initializer=RandomNormal(mean=0., stddev=1.), **kwargs)
+    def __init__(self, units, gain=2, lrmul=1, **kwargs):
+        super(DenseEQL, self).__init__(kwargs)
+        self.units = units
         self.gain = gain
+        self.lrmul = lrmul
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({'units': self.units})
+        config.update({'gain': self.gain})
+        config.update({'lrmul': self.lrmul})
+        return config
 
     def build(self, input_shape):
-        super().build(input_shape)
-        fan_in = np.prod(input_shape[1:])
-        he_const = self.gain / np.sqrt(fan_in)
-        self.kernel = self.kernel * he_const
+        self.in_channels = input_shape[-1]
+        self.w = self.add_weight(
+            shape=[self.in_channels, self.units],
+            initializer=RandomNormal(mean=0.0, stddev=1.0 / self.lrmul),
+            trainable=True,
+            name='kernel'
+        )
+        self.b = self.add_weight(
+            shape=(self.units,),
+            initializer='zeros',
+            trainable=True,
+            name='bias'
+        )
+        fan_in = self.in_channels
+        self.scale = tf.sqrt(self.gain/fan_in)
 
+    @tf.function
+    def call(self, inputs):
+        output = tf.matmul(inputs, self.scale * self.w) + self.b
+        return output * self.lrmul
 
-class Conv2DEQL(Conv2D):
+class Conv2DEQL(Layer):
 
-    def __init__(self, gain=np.sqrt(2), **kwargs):
-        super().__init__(kernel_initializer=RandomNormal(mean=0., stddev=1.), **kwargs)
+    def __init__(self, out_channels, kernel=3, gain=2, **kwargs):
+        super(Conv2DEQL, self).__init__(kwargs)
+        self.kernel = kernel
+        self.out_channels = out_channels
         self.gain = gain
+        self.pad = self.kernel != 1
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({'kernel': self.kernel})
+        config.update({'out_channels': self.out_channels})
+        config.update({'gain': self.gain})
+        config.update({'pad': self.pad})
+        return config
 
     def build(self, input_shape):
-        super().build(input_shape)
-        fan_in = np.prod(input_shape[1:])
-        he_const = self.gain / np.sqrt(fan_in)
-        self.kernel = self.kernel * he_const
+        self.in_channels = input_shape[-1]
+        initializer = RandomNormal(mean=0.0, stddev=1.0)
+        self.w = self.add_weight(
+            shape=[self.kernel, self.kernel, self.in_channels, self.out_channels],
+            initializer=initializer,
+            trainable=True,
+            name='kernel'
+        )
+        self.b = self.add_weight(
+            shape=(self.out_channels,),
+            initializer='zeros',
+            trainable=True,
+            name='bias'
+        )
+        fan_in = self.kernel * self.kernel * self.in_channels
+        self.scale = tf.sqrt(self.gain/fan_in)
+
+    def call(self, inputs):
+        if self.pad:
+            x = tf.pad(inputs, [[0, 0], [1, 1], [1, 1], [0, 0]], mode='REFLECT')
+        else:
+            x = inputs
+        output = tf.nn.conv2d(x, self.scale * self.w, strides=1, padding="VALID") + self.b
+        return output
 
 
 class AdaptiveInstanceModulation(Layer):

processing/image_augmenter.py
@@ -9,12 +9,13 @@ WAVELET = [0.015404109327027373, 0.0034907120842174702, -0.11799011114819057, -0
 
 class ImageAugmenter:
 
-    def __init__(self, init_p_augment=0.00):
-        # Augmentation parameters.
-        self.p_augment = init_p_augment
-        self.n_adjust_imgs = 500000
+    def __init__(self):
+        # Global augmentation parameters.
+        self.p_augment = 0.00
         self.p_augment_target = 0.50
         self.p_augment_threshold = 0.01
+        self.p_augment_max = 0.80
+        self.n_adjust_imgs = 500000
         # Geometric transformation probabilities.
         self.p_flip = 1.00
         self.p_90d_rotation = 1.00
@@ -55,7 +56,7 @@ class ImageAugmenter:
     # ---------------------------------------- Augmentation Probability ------------------------------------------------
     def adapt_augmentation_probability(self, rt, n_shown_images):
         p_augment = self.p_augment + (n_shown_images/self.n_adjust_imgs) * np.sign(rt - self.p_augment_target)
-        self.p_augment = np.clip(p_augment, 0.0, 1.0)
+        self.p_augment = np.clip(p_augment, 0.0, self.p_augment_max)
 
     # ---------------------------------------- Geometric Transforms ----------------------------------------------------
     def _apply_geometric_transforms(self, g_inv, x, width, height, channels):
@@ -259,15 +260,16 @@ class ImageAugmenter:
         return c_sat_scale @ c
 
     # ---------------------------------------- Matrix Construction -----------------------------------------------------
-
-    def _apply_probability_mask(self, probability, parameters, mask_value):
+    @staticmethod
+    def _apply_probability_mask(probability, parameters, mask_value):
         shape = tf.shape(parameters)
         mask = tf.random.uniform(shape, 0, 1) < probability
         mask_value = tf.broadcast_to(tf.convert_to_tensor(mask_value, dtype=parameters.dtype), shape)
         masked_parameters = tf.where(mask, parameters, mask_value)
         return masked_parameters
 
-    def _convert_rows_to_matrix(self, *rows):
+    @staticmethod
+    def _convert_rows_to_matrix(*rows):
         rows = [[tf.convert_to_tensor(x, dtype=tf.float32) for x in r] for r in rows]
         batch_elems = [x for r in rows for x in r if x.shape.rank != 0]
         assert all(x.shape.rank == 1 for x in batch_elems)

training/fid_calculator.py
@@ -7,19 +7,27 @@ from tensorflow.keras.applications.inception_v3 import InceptionV3
 
 class FIDCalculator:
 
-    def __init__(self, image_processor, fid_res=128, n_fake_samples=128, n_real_samples=10000):
-        print(f"Constructing Frechet Inception Distance calculator...")
+    def __init__(self, image_processor, fid_res=128, n_fake_samples=128, n_real_samples=10000, n_activations=2048):
+        print(f"Constructing FID calculator...")
         self.image_processor = image_processor
         self.n_fake_samples = n_fake_samples
         self.n_real_samples = n_real_samples
+        self.n_activations = n_activations
+        self.chunk_size = 100
         self.img_shape = (fid_res, fid_res)
         self.fid_shape = (fid_res, fid_res, 3)
         self.inception_network = InceptionV3(include_top=False, pooling='avg', input_shape=self.fid_shape)
-        self.mu_real, self.cov_real, self.trc_cov_real = self._precompute_values()
+        self.mu_real, self.cov_real, self.trc_cov_real = self._precompute_reals()
 
     def compute_fast_fid(self, generator):
         # 1. Generate fake images.
-        x_fake = generate_fake_samples(self.image_processor, generator, self.n_fake_samples, self.img_shape, transform_type="new_to_zero_one")
+        x_fake = generate_fake_samples(
+            self.image_processor,
+            generator,
+            self.n_fake_samples,
+            self.img_shape,
+            transform_type="new_to_zero_one"
+        )
         # 2. Compute activations on fake images.
         a_fake = np.transpose(self.inception_network.predict(x_fake))
         mu_fake = a_fake.mean(axis=1).reshape(-1, 1)
@@ -53,17 +61,31 @@ class FIDCalculator:
             cov_diff = cov_diff.real
         return mu_diff + np.trace(cov_real + cov_fake - 2.0 * cov_diff)
 
-    def _precompute_values(self):
-        x_real = generate_real_samples(self.image_processor, self.n_real_samples, self.img_shape, transform_type="old_to_zero_one")
-        a_real = self.inception_network.predict(x_real)
-        mu_real, cov_real = a_real.mean(axis=0).reshape(-1, 1), np.cov(a_real, rowvar=False)
+    def _precompute_reals(self):
+        mu_real, cov_real = self._cov_mean_low_memory()
         trc_cov_real = np.trace(cov_real)
         return mu_real, cov_real, trc_cov_real
+
+    def _cov_mean_low_memory(self):
+        n_steps = self.n_real_samples // self.chunk_size
+        mu_loop = np.zeros((self.n_activations, 1))
+        cov_loop = np.zeros((self.n_activations, self.n_activations))
+        for _ in range(n_steps):
+            x_real = generate_real_samples(self.image_processor, self.chunk_size, self.img_shape, transform_type="old_to_zero_one")
+            a_real = self.inception_network.predict(x_real)
+            for k in range(self.chunk_size):
+                a_real_k = a_real[[k], :].reshape(-1, 1)
+                mu_loop += a_real_k
+                cov_loop += np.outer(a_real_k, a_real_k)
+        mu_loop /= (self.n_real_samples-1)
+        cov_loop /= (self.n_real_samples-1)
+        cov_loop -= np.outer(mu_loop, mu_loop)
+        return mu_loop, cov_loop
 
     def _compute_trace_sqrt_mat(self, c_fake):
         mat_m = np.matmul(np.transpose(c_fake), np.matmul(self.cov_real, c_fake))
         eig_values, _ = np.linalg.eig(mat_m)
-        trc = np.sum(np.abs(np.sqrt(eig_values+10e-8)))
+        trc = np.sum(np.abs(np.sqrt(np.abs(eig_values))))
         return trc
 
     @staticmethod

training/training_evaluator.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 class TrainingEvaluator:
 
     def __init__(self, image_processor=None):
-        self.loss_burn_in = 1
+        self.loss_burn_in = 10
         self.fid_burn_in = 0
         self.image_processor = image_processor
         self.d_loss_total = 'd_loss_total'
@@ -26,11 +26,11 @@ class TrainingEvaluator:
         plt.subplot(2, 3, 1)
         d_loss_real = self._scale_losses(loss_dict[self.d_loss_real][self.loss_burn_in:])
         d_loss_fake = self._scale_losses(loss_dict[self.d_loss_fake][self.loss_burn_in:])
-        d_loss = self._scale_losses(self._add_d_losses(loss_dict)[self.loss_burn_in:])
+        d_loss_total = self._scale_losses(loss_dict[self.d_loss_total][self.loss_burn_in:])
         plt.title("Discriminator Loss")
         plt.plot(d_loss_real, label='discriminator loss (real)', color='orange', linewidth=0.25)
         plt.plot(d_loss_fake, label='discriminator loss (fake)', color='blue', linewidth=0.25)
-        plt.plot(d_loss, label='discriminator loss (total)', color='black', linewidth=0.25)
+        plt.plot(d_loss_total, label='discriminator loss (total)', color='black', linewidth=0.25)
         plt.grid()
         plt.tight_layout()
         plt.subplot(2, 3, 2)
@@ -102,7 +102,7 @@ class TrainingEvaluator:
         return {k: [dic[k] for dic in lod] for k in lod[0]}
 
     @staticmethod
-    def _scale_losses(losses, scale_method="e"):
+    def _scale_losses(losses, scale_method="none"):
         if scale_method == "square_root":
             return [np.sqrt(x) if x > 0 else -np.sqrt(-x) for x in losses]
         else:
@@ -115,5 +115,5 @@ class TrainingEvaluator:
         return losses
 
 
-dir_loss = f"C:\\Users\\robin\\Desktop\\Projects\\painter\\io\\output\\bob_ross_128x128"
+dir_loss = f"C:\\Users\\robin\\Desktop\\Projects\\painter\\io\\output\\bob_ross_64x64"
 TrainingEvaluator().plot_loss(dir_loss)